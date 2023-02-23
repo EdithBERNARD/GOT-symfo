69b6222ed59cb19735f06aa3e45c48147a7199a6
EdithBERNARD <edith.bernard80@gmail.com>
2023-02-23 22:08:48 +0100

    page perso - ok pour standard et maison - pb rang et parents / houses ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Title;
 use App\Repository\CharacterRepository;
+use App\Repository\HouseRepository;
+use App\Repository\TitleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,8 +21,11 @@ class HomeController extends AbstractController
     public function index(CharacterRepository $characterRepository): Response
     {
 
-        //todo je recupère tout mes perso
+        //todo je recupère tous mes perso
         $allCharacters = $characterRepository->findAll();
+
+        
+
         return $this->render('home/index.html.twig', [
             "allCharacters" => $allCharacters
         ]);
@@ -31,16 +37,43 @@ class HomeController extends AbstractController
      * 
      * @return Reponse
      */
-    public function show($index, CharacterRepository $characterRepository): Response
+    public function show($index, CharacterRepository $characterRepository,HouseRepository $houseRepository): Response
     {
 
         //todo je cherche le bon perso dans ma BDD
         $character = $characterRepository->find($index);
+        //todo je recupère tous mes perso
+        //$allCharacters = $characterRepository->findAll();
+
+        //todo je recupère toutes mes maisons
+        $allHouses = $houseRepository->findAll();
+
+        //$allTitles = $titleRepository->findBy([]);
+
         return $this->render('home/show.html.twig', [
-            "theCharacter" => $character
+            "theCharacter" => $character,
+            "allHouses" => $allHouses,
+            //"allCharacters" => $allCharacters,
+            //"allTitles" => $allTitles
+            
         ]);
     }
     //TODO : route "" : affiche l'ensmble des maisons
+    // TODO : route index : affiche l'ensemble des perso
+    /**
+     * @Route("/house", name="app_home_list")
+     * 
+     * @return Reponse
+     */
+    public function list(HouseRepository $houseRepository): Response
+    {
+
+        //todo je recupère toutes mes maisons
+        $allHouses = $houseRepository->findAll();
+        return $this->render('home/houses.html.twig', [
+            "allHouses" => $allHouses
+        ]);
+    }
 
     //TODO : route "" : affiche les perso d'une maison (id maison)
 }
